800a64007559a9227d63e11f7cde2b75c63076b2
itssuhan <suhan.ashraf@learner.manipal.edu>
2023-03-05 01:06:08 +0530

    lab files updated

--- DBS Lab/Lab 3/SQLQueries1-20.sql
+++ DBS Lab/Lab 3/SQLQueries1-20.sql
@@ -175,7 +175,8 @@ Finance
 
 --Q13. Find all courses taught in both the Fall 2009 semester and in the Spring 2010 semester.
 
-select course_id from section where semester='Fall' and year=2009 and exists(select course_id from section where semester = 'Spring' and year=2010);
+select c.course_id,c.title from course c,section s where c.course_id=s.course_id and s.semester='Fall' and s.year=2009 and exists
+(select * from section s where c.course_id=s.course_id and s.semester='Spring' and s.year=2010);
 
 /*
 
@@ -189,3 +190,94 @@ PHY-101
 
 --Q14.  Find all students who have taken all courses offered in the Biology department.
 
+select s.name from student s where exists(select * from takes t where s.id=t.id and s.dept_name='Biology');
+
+/*
+
+NAME
+--------------------
+Tanaka
+
+*/
+
+-- 15. Find all courses that were offered at most once in 2009. --
+
+select unique course_id from course c where 1>=(select count(course_id) from section s where c.course_id=s.course_id and s.year=2009 group by course_id);
+
+/*
+
+COURSE_I
+--------
+BIO-101
+CS-101
+CS-347
+EE-181
+PHY-101
+
+*/
+
+-- 16. Find all the students who have opted at least two courses offered by CSE department --
+
+select name,dept_name from student s where 2<=(select count(id) from takes t where s.id=t.id and s.dept_name='Comp. Sci.' group by id);
+
+/*
+
+ID    NAME                 DEPT_NAME              TOT_CRED
+----- -------------------- -------------------- ----------
+00128 Zhang                Comp. Sci.                  102
+12345 Shankar              Comp. Sci.                   32
+54321 Williams             Comp. Sci.                   54
+76543 Brown                Comp. Sci.                   58
+
+*/
+
+-- 17. Find the average instructors salary of those departments where the average salary is greater than 42000 --
+
+select dept_name,AVG_SAL from (select dept_name,avg(salary) as AVG_SAL from instructor group by dept_name having avg(salary)>42000);
+
+/*
+
+DEPT_NAME               AVG_SAL
+-------------------- ----------
+Elec. Eng.                80000
+Physics                   91000
+Comp. Sci.           77333.3333
+Finance                   85000
+Biology                   72000
+History                   61000
+
+*/
+
+-- 18. Create a view all_courses consisting of course sections offered by Physics department in the Fall 2009, with the building and room number of each section.
+
+create view Phy_Dept as select building,room_number from section natural join course where dept_name='Physics' and semester='Fall' and year=2009;
+select * from Phy_Dept;
+
+/*
+ 
+BUILDING        ROOM_NU
+--------------- -------
+Watson          100
+ 
+ */
+
+--20. Create a view department_total_salary consisting of department name and total salary of that department
+
+create view dept_tot_sal as select dept_name,sum(salary) tot_salary from instructor group by dept_name;
+select * from dept_tot_sal;
+
+ /*
+ 
+DEPT_NAME            TOT_SALARY
+-------------------- ----------
+Elec. Eng.                80000
+Physics                  182000
+Comp. Sci.               232000
+Finance                  170000
+Biology                   72000
+Music                     40000
+History                  122000
+ 
+ */
+
+ 
